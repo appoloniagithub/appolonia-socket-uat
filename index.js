5ea514cfe585c4ad96f23f89519632fb26772353
tasduqali <tasduqali2@gmail.com>
2023-01-02 18:00:50 +0400

    createdat

--- index.js
+++ index.js
@@ -88,15 +88,8 @@ io.on("connection", (socket) => {
 
   // send message to a specific user
   socket.on("send-message", (data) => {
-    const {
-      senderId,
-      receiverId,
-      message,
-      conversationId,
-      format,
-      scanId,
-      createdAt,
-    } = data;
+    const { senderId, receiverId, message, conversationId, format, scanId } =
+      data;
     const user = activeUsers.find((user) => user.userId === receiverId);
     console.log("Sending from socket to :", receiverId);
     console.log("Data in send message: ", data);
